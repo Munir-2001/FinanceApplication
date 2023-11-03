fcbd63b72616394dc00246a5ef25a66d0fdb8dd5
Munir Abbasi <87448868+Munir-2001@users.noreply.github.com>
2023-11-03 15:37:26 +0500

    Update Program.cs

--- FinanceApplication/Program.cs
+++ FinanceApplication/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MVCdbdemo>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MVCconnstring")));
