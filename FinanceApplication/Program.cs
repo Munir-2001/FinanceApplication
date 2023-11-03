9f7e13755d90983c52ea629c53f854af77f1ac77
Munir Abbasi <87448868+Munir-2001@users.noreply.github.com>
2023-11-03 15:38:10 +0500

    Update Program.cs

--- FinanceApplication/Program.cs
+++ FinanceApplication/Program.cs
@@ -21,7 +21,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
